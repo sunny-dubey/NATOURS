32bf1256ad2e048bbae543d351618cc742f3f6b4
sunny-dubey <97658562+sunny-dubey@users.noreply.github.com>
2023-08-01 23:25:22 +0530

    mongodb connected

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(con => {
+  console.log('DB connection successful!');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
